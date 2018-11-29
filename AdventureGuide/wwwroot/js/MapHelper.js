b4f578ec2853b47736eeabc9aa421cb01dd9c7ed
David Wetzel <squirrtlez@ufl.edu>
2018-11-29 14:46:56 -0500

    Added Multiple Pins to a Map

--- AdventureGuide/wwwroot/js/MapHelper.js
+++ AdventureGuide/wwwroot/js/MapHelper.js
@@ -9,25 +9,60 @@ function InitMap() {
     ChangeMarkerPosition();
 }
 function ChangeMarkerPosition() {
-    var country = $("#Country").val();
-    var state = $("#State").val();
-    var city = $("#City").val();
-    var address = $("#Address").val();
-    var fullAddress = country + "," + state + "," + city + "," + address;
-
-    geocoder.geocode({ 'address': fullAddress }, function (results, status) {
-        if (status === google.maps.GeocoderStatus.OK) {
-            markersLayer.clearLayers();
-            var lat = results[0].geometry.location.lat();
-            var lon = results[0].geometry.location.lng();
-            var marker = L.marker([lat, lon]);
-            markersLayer.addLayer(marker);
-            setTimeout(function () {
-                map.invalidateSize();
-                map.panTo(new L.LatLng(lat, lon));
-            }, 100);
-        } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-        }
+    //var country = $("#Country").val();
+    //var state = $("#State").val();
+    //var city = $("#City").val();
+    //var address = $("#Address").val();
+    //var fullAddress = country + "," + state + "," + city + "," + address;
+    var latArray = new Array();
+    var lonArray = new Array();
+    $("input.hidden-lat").each(function () {
+            latArray.push($(this).val());
+            
     });
+    $("input.hidden-lon").each(function () {
+        lonArray.push($(this).val());
+    });
+    
+    var markerArray = new Array();
+    var lat = 0;
+    var lon = 0;
+    for (var i = 0; i < latArray.length; i++) {
+        //console.log(latArray[i]);
+        //console.log(lonArray[i]);
+        lat += parseFloat(latArray[i]);
+        lon += parseFloat(lonArray[i]);
+        markerArray.push(L.marker([latArray[i], lonArray[i]]));
+    }
+    lat = lat / latArray.length;
+    lon = lon / lonArray.length;
+    var markers = L.layerGroup(markerArray);
+
+    //var lat = $("#Lat").val();
+    //var lon = $("#Lon").val();
+    markersLayer.clearLayers();
+    //marker = L.marker([lat, lon]);
+    markersLayer.addLayer(markers);
+    
+    setTimeout(function () {
+        map.invalidateSize();
+        map.panTo(new L.LatLng(lat, lon));
+    }, 100);
+    //geocoder.geocode({ 'address': fullAddress }, function (results, status) {
+    //    if (status === google.maps.GeocoderStatus.OK) {
+    //        markersLayer.clearLayers();
+    //        var lat = results[0].geometry.location.lat();
+    //        var lon = results[0].geometry.location.lng();
+    //        console.log(lat);
+    //        console.log(lon);
+    //        var marker = L.marker([lat, lon]);
+    //        markersLayer.addLayer(marker);
+    //        setTimeout(function () {
+    //            map.invalidateSize();
+    //            map.panTo(new L.LatLng(lat, lon));
+    //        }, 100);
+    //    } else {
+    //        alert('Geocode was not successful for the following reason: ' + status);
+    //    }
+    //});
 }
