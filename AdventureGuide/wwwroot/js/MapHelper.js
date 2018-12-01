142d2cf14fdfb5f4a3f4ad70b4a4f6d1759f37cf
David Wetzel <squirrtlez@ufl.edu>
2018-12-01 17:10:31 -0500

    progress towards showing complete info on a pin

--- AdventureGuide/wwwroot/js/MapHelper.js
+++ AdventureGuide/wwwroot/js/MapHelper.js
@@ -11,16 +11,45 @@ function ChangeMarkerPosition() {
 
     var latArray = new Array();
     var lonArray = new Array();
+    var nameArray = new Array();
+    var ratingArray = new Array();
+    var idArray = new Array();
+
     $("input.hidden-lat").each(function () {
             latArray.push($(this).val());
-            
     });
     $("input.hidden-lon").each(function () {
         lonArray.push($(this).val());
     });
+    $("input.hidden-name").each(function () {
+        nameArray.push($(this).val());
+    });
+    $("input.hidden-rating").each(function () {
+        ratingArray.push($(this).val());
+    });
+    $("input.hidden-id").each(function () {
+        idArray.push($(this).val());
+    });
+
     var markerArray = new Array();
     for (var i = 0; i < latArray.length; i++) {
-        markerArray.push(L.marker([latArray[i], lonArray[i]]));
+        var mark = L.marker([latArray[i], lonArray[i]]);
+        if (nameArray.length === 0) {
+            mark.bindPopup();
+        }
+        else {
+            var id = "input-" + idArray[i];
+            var onclick = "InitRating(this)";
+            var startDiv = "<div class='my-star' data-string='" + id + "' onclick='" + onclick + "'>";
+            var nameDiv = "<h2>" + nameArray[i] + "</h2>";
+            var inputDiv = "<input id='" + id + "' name='"+ id + "' class='rating rating-loading'" + " value='" + ratingArray[i] + "' data-min='0' data-max='5' data-step='.5' data-readonly='true' />";
+            var endDiv = "</div>";
+            var markInfo = startDiv + nameDiv + inputDiv + endDiv;
+            console.log(markInfo);
+            mark.bindPopup(markInfo);
+        }
+
+        markerArray.push(mark);
     }
     var markers = L.layerGroup(markerArray);
     var group = new L.featureGroup(markerArray);
